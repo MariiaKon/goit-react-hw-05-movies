9c00e3ccd7046ae339b7565c2a7093c1977a98a2
MariiaKon <71904680+MariiaKon@users.noreply.github.com>
2022-05-27 21:28:41 +0000

    Deploying to gh-pages from @ MariiaKon/goit-react-hw-05-movies@25a1ddc3584d1e2576294f63701bb2db241c08b3 🚀

---
+++ static/js/819.2cae87f6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[819],{2819:function(n,e,t){t.r(e),t.d(e,{default:function(){return u}});var r=t(5024),a=t(184);function u(){return(0,a.jsx)(r.a,{children:"Loading..."})}},5024:function(n,e,t){t.d(e,{a:function(){return u}});var r,a=t(168),u=t(6031).ZP.div(r||(r=(0,a.Z)(["\n  font-size: 20px;\n  letter-spacing: 0.05em;\n  padding: 20px 0;\n"])))}}]);
+//# sourceMappingURL=819.2cae87f6.chunk.js.map
